d513d9798d66b6a2bd3b41b40fa33c078c335d8a
Felipe Vianna <felipe.vianna86@gmail.com>
2024-02-27 23:14:28 -0300

    Commit initial

---
+++ src/test/java/com/example/avengers/avengersapi/AvengersApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.avengers.avengersapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AvengersApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
